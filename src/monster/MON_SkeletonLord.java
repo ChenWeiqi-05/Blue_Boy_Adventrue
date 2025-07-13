e71b4d21243c7e4f818a164a74f8b6f87528ab63
chenweiqi <2292007239@qq.com>
2025-07-13 11:16:44 +0800

    1 实现了游戏的存档 2 实现了游戏的地牢探索 3 实现了地牢的解密 4.1 monster蝙蝠的运动逻辑

--- src/monster/MON_SkeletonLord.java
+++ src/monster/MON_SkeletonLord.java
@@ -1,6 +1,108 @@
 package monster;
 
 import Entity.Entity;
+import main.GamePanel;
+import object.OBJ_Coin_Bronze;
+import object.OBJ_Heart;
+import object.OBJ_ManaCrystal;
+
+import java.util.Random;
 
 public class MON_SkeletonLord extends Entity {
+    GamePanel gp;
+    public MON_SkeletonLord(GamePanel gp) {
+        super(gp);
+        this.gp = gp;
+        type = type_monster;
+        name = "SkeletonLord";
+
+        defaultSpeed = 1;
+        speed = defaultSpeed;
+        maxLife = 10;
+        life = maxLife;
+        attack = 2;
+        defense = 2;
+        exp = 10;
+
+        knockBackPower = 5;
+
+
+        solidArea.x = 4;
+        solidArea.y = 4;
+        solidArea.width = 40;
+        solidArea.height = 44;
+        solidAreaDefaultX = solidArea.x;
+
+        attackArea.width = 48;
+        attackArea.height = 48;
+
+        motion1_duration = 40;
+        motion2_duration = 85;
+
+        getImage();
+        getAttackImage();
+    }
+
+    public void getImage() {
+        up1 = setup("/monster/orc_up_1", gp.tileSize, gp.tileSize);
+        up2 = setup("/monster/orc_up_2", gp.tileSize, gp.tileSize);
+
+        down1 = setup("/monster/orc_down_1", gp.tileSize, gp.tileSize);
+        down2 = setup("/monster/orc_down_2", gp.tileSize, gp.tileSize);
+
+        left1 = setup("/monster/orc_left_1", gp.tileSize, gp.tileSize);
+        left2 = setup("/monster/orc_left_2", gp.tileSize, gp.tileSize);
+
+        right1 = setup("/monster/orc_right_1", gp.tileSize, gp.tileSize);
+        right2 = setup("/monster/orc_right_2", gp.tileSize, gp.tileSize);
+    }
+
+    public void getAttackImage() {
+        attackUp1 = setup("/monster/orc_attack_up_1", gp.tileSize, gp.tileSize * 2);
+        attackUp2 = setup("/monster/orc_attack_up_2", gp.tileSize, gp.tileSize * 2);
+        attackDown1 = setup("/monster/orc_attack_down_1", gp.tileSize, gp.tileSize * 2);
+        attackDown2 = setup("/monster/orc_attack_down_2", gp.tileSize, gp.tileSize * 2);
+        attackLeft1 = setup("/monster/orc_attack_left_1", gp.tileSize * 2, gp.tileSize);
+        attackLeft2 = setup("/monster/orc_attack_left_2", gp.tileSize * 2, gp.tileSize);
+        attackRight1 = setup("/monster/orc_attack_right_1", gp.tileSize * 2, gp.tileSize);
+        attackRight2 = setup("/monster/orc_attack_right_2", gp.tileSize * 2, gp.tileSize);
+    }
+    public void setAction() {
+
+        if (onPath == true){
+            checkStopChasingOrNot(gp.player, 15, 100);
+
+            searchPath(getGoalCol(gp.player), getGoalRow(gp.player));
+
+            checkShootOrNot(200, 30);
+        }
+        else {
+            checkStartChasingOrNot(gp.player,5,100);
+
+            getRandomDirection(120);
+        }
+        if (attacking == false){
+            checkAttackOrNot(30, gp.tileSize*4, gp.tileSize);
+        }
+
+    }
+    public void damageReaction() {//史莱姆受到攻击后的退后ai
+        actionLockCounter = 0;
+        // direction = gp.player.direction1;
+        onPath = true;
+    }
+    public void checkDrop() {//这段代码控制物品的随机掉落物品
+
+        int i = new Random().nextInt(100) + 1;
+
+        if (i < 50) {//当随机数字在0到50之间时，掉落金币
+            dropItem(new OBJ_Coin_Bronze(gp));
+        }
+        if (i >= 50 && i < 75) {//当随机数字在50到75之间时，掉落生命
+            dropItem(new OBJ_Heart(gp));
+        }
+        if (i >= 75 && i < 100) {//当随机数字在75到100之间时，掉落魔法水晶
+            dropItem(new OBJ_ManaCrystal(gp));
+        }
+    }
 }
